abe66b71337c8ec8dd525a48ac7d94a7e6f2b2b0
Jogis <ronipihajoki@proton.me>
2025-12-03 20:37:18 +0200

    Update App.java

--- LeluLuokka/src/App.java
+++ LeluLuokka/src/App.java
@@ -34,3 +34,25 @@ class vieteriauto {
             }
 
     }
+
+    /*Tee kantaluokka Lelu, jolla on kaksi attribuuttia (esim. nimi ja paino), parametriton muodostin,
+    yksi muodostin kaikilla parametreilla ja metodi, joka tulostaa olion tiedot.
+
+Tee toinen luokka - Vieteriauto, joka perii yllä tehdyn Lelu-kantaluokan.
+
+Tee Vieteriauto-luokalle tarvittavat muodostimet.
+
+Määrittele Vieteriauto-luokkaan attribuutti nopeus ja kiihdyta-metodi, joka kasvattaa nopeus-attribuutin arvoa yhdellä.
+
+Tee Vieteriauto-luokalle metodi, joka tulostaa kaikki olion tiedot. Nimeä metodi saman nimiseksi kuin Lelu-luokan
+“tietojen tulostus” -metodi. Ylikirjoita (override) Vieteriauton “tietojen tulostus” -metodi.
+
+Tee olio Lelu-luokasta ja tulosta sen tiedot.
+
+Tee olio Vieteriauto-luokasta ja tulosta sen tiedot.
+
+Muuta jokin kantaluokan attribuutti privateksi (esim. paino). Mitä tapahtuu?
+
+Entä, jos muutat Lelu-luokan “tietojen tulostus” -metodin näkyvyydeksi private...?
+
+Jos intoa vielä riittää, periytä Lelu-luokasta jokin toinen luokka. */
