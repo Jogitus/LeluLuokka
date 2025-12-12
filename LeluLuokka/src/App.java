cdf3a84bf6ab3a85808d169663dedd6befbd3ebc
Jogis <ronipihajoki@proton.me>
2025-12-12 21:12:34 +0200

    Update App.java
    Oon unohtanu kaiken.
    Toivo  on menetetty.
    Aika lukea Javasta kirja, kun ei videoista oo apua tässä ahdingossa.

--- LeluLuokka/src/App.java
+++ LeluLuokka/src/App.java
@@ -1,52 +1,28 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        lelu lo1 = new lo1("Miele", 300);
-        lelu lo2 = new lo2();
-        vieteriauto vao1 = new vao1();
+        Lelu olio1 = new Lelu("Ferru", 30); // Tän pitäs kai tehä jotain
+
+    leluolio1.tulostaTiedot();
     }
-} // Main Loppuu
-class lelu {
+} // Main Loppuu 
+
+class Lelu { // Luokka Lelu
     
     public String nimi;
     public int paino;
 
-            public void tulostaTiedot(String nimi, int paino)
+            public leluauto(String nimi, int paino) // Luokka Lelu methodi vai olio?
             {
+                this.nimi = nimi;
+                this.paino = paino;
+            }
+    }
+
+
+            public void tulostaTiedot(){ // Luokka Lelu methodi vai olio?
                 System.out.println("Auton nimi: " + nimi);
-                System.out.println("Auton paino: " + paino " grammaa");
+                System.out.println("Auton paino: " + paino + " grammaa");
             }
-    }
-class vieteriauto {
-    int nopeus = 0;
-            public void kiihdyta()
-            {
-                for(int i = 0, i<5, i++){
-                    nopeus++;
-            }
-            public void tulostaTiedot()
-            {
-            
-            }
-    }
-/*Tee kantaluokka Lelu, jolla on kaksi attribuuttia (esim. nimi ja paino), parametriton muodostin,
-yksi muodostin kaikilla parametreilla ja metodi, joka tulostaa olion tiedot.
 
-Tee toinen luokka - Vieteriauto, joka perii yllä tehdyn Lelu-kantaluokan.
-
-Tee Vieteriauto-luokalle tarvittavat muodostimet.
-
-Määrittele Vieteriauto-luokkaan attribuutti nopeus ja kiihdyta-metodi, joka kasvattaa nopeus-attribuutin arvoa yhdellä.
-
-Tee Vieteriauto-luokalle metodi, joka tulostaa kaikki olion tiedot. Nimeä metodi saman nimiseksi kuin Lelu-luokan
-“tietojen tulostus” -metodi. Ylikirjoita (override) Vieteriauton “tietojen tulostus” -metodi.
-
-Tee olio Lelu-luokasta ja tulosta sen tiedot.
-
-Tee olio Vieteriauto-luokasta ja tulosta sen tiedot.
-
-Muuta jokin kantaluokan attribuutti privateksi (esim. paino). Mitä tapahtuu?
-
-Entä, jos muutat Lelu-luokan “tietojen tulostus” -metodin näkyvyydeksi private...?
-
-Jos intoa vielä riittää, periytä Lelu-luokasta jokin toinen luokka. */
+            // Vittu että masentaa
 
