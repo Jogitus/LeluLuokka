2aef970e78ea382b2766aef3579555bbc5766f6a
Jogis <ronipihajoki@proton.me>
2026-01-22 20:58:41 +0200

    .
    .

--- LeluLuokka/src/App.java
+++ LeluLuokka/src/App.java
@@ -1,28 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Lelu olio1 = new Lelu("Ferru", 30); // Tän pitäs kai tehä jotain
 
-    leluolio1.tulostaTiedot();
+   
     }
 } // Main Loppuu 
-
-class Lelu { // Luokka Lelu
-    
-    public String nimi;
-    public int paino;
-
-            public leluauto(String nimi, int paino) // Luokka Lelu methodi vai olio?
-            {
-                this.nimi = nimi;
-                this.paino = paino;
-            }
-    }
-
-
-            public void tulostaTiedot(){ // Luokka Lelu methodi vai olio?
-                System.out.println("Auton nimi: " + nimi);
-                System.out.println("Auton paino: " + paino + " grammaa");
-            }
-
-            // Vittu että masentaa
 
